9c3874e2d78f784825cbaedc7c29683e5f27b647
Pedro Aquino <pedro.aquino479@gmail.com>
2023-08-07 02:21:11 +0200

    add change to .git dir

--- testUsecases/test_framework_v2.py
+++ testUsecases/test_framework_v2.py
@@ -1,31 +1,40 @@
 import subprocess, os
-
+from pathlib import Path
 
 def remove_docker_containers():
     print("Removing all docker containers")
     try:
         containers = subprocess.check_output(['docker', 'ps', '-a', '-q']).decode().strip().split('\n')
         if not containers:
-            print("No running containers found.")
             return
         for container in containers:
             stop_command = "docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)"
             subprocess.run(stop_command, shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            print("All docker containers removed")
     except subprocess.CalledProcessError as e:
         print("")
 
+def change_to_root_dir():
+    current_dir = os.getcwd()
+    while current_dir != '/':
+        if '.git' in os.listdir(current_dir):
+            os.chdir(current_dir)
+            return
+        current_dir = os.path.dirname(current_dir)
+
+
 
 def deploy_fabric_network():
+    change_to_root_dir()
     print("Deploying Fabric Network")
-    os.chdir("../Masters-v2/Fabric/fabric-network-cc/")
+    os.chdir("Masters-v2/Fabric/fabric-network-cc/")
     command = 'python3 deploy_fabric.py'
     process = subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print("Fabric Network deployed")
 
 def deploy_ipfs_node():
+    change_to_root_dir()
     print("Deploying IPFS Node")
-    os.chdir("../../IPFS/")
+    os.chdir("Masters-v2/IPFS/")
     command = 'docker run -it -d \
                 --name ipfs \
                 --network docker_default \
@@ -37,19 +46,20 @@ def deploy_ipfs_node():
     print("IPFS Node deployed")
 
 def deploy_gateway():
+    change_to_root_dir()
     print("Deploying Gateway")
-    os.chdir("../Gateway/")
+    os.chdir("Masters-v2/Gateway/")
     command = 'python3 initGateway.py'
     process = subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print("Gateway deployed")
 
 def deploy_smartdevice():
+    change_to_root_dir()
     print("Deploying Smart Device")
-    os.chdir("../SmartDevice/")
+    os.chdir("Masters-v2/SmartDevice/")
     command = 'python3 initSD.py'
     process = subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print("Smart Device deployed")    
-
 
 
 
@@ -64,10 +74,10 @@ def main():
     deploy_ipfs_node()
 
     #DEPLOY GATEWAY
-    deploy_gateway()
+    #deploy_gateway()
 
     #DEPLOY SMART DEVICE
-    deploy_smartdevice()
+    #deploy_smartdevice()
 
 
 
