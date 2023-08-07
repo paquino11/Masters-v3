938f8cb2fe1723c4bca00a935f3030ed6bc96107
Pedro Aquino <pedro.aquino479@gmail.com>
2023-08-07 02:46:39 +0200

    add time execution

--- testUsecases/AgentsDeployment/deploy_agents.py
+++ testUsecases/AgentsDeployment/deploy_agents.py
@@ -1,8 +1,10 @@
 import subprocess, os
+import test_framework_v2
 
 def deploy_consortium():
-    print("Deploying COnsortium Agent")
-    os.chdir("../Masters-v2/Fabric/fabric-network-cc/")
-    command = 'python3 deploy_fabric.py'
+    test_framework_v2.change_to_root_dir()
+    print("Deploying Consortium Agent")
+    os.chdir("Agents/")
+    command = 'LEDGER_URL=http://dev.greenlight.bcovrin.vonx.io ./run consortium'
     process = subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print("Fabric Network deployed")
--- testUsecases/test_framework_v2.py
+++ testUsecases/test_framework_v2.py
@@ -1,5 +1,7 @@
 import subprocess, os
 from pathlib import Path
+import docker
+import time
 
 def remove_docker_containers():
     print("Removing all docker containers")
@@ -21,64 +23,104 @@ def change_to_root_dir():
             return
         current_dir = os.path.dirname(current_dir)
 
+def are_containers_deployed(container_names):
+    try:
+        client = docker.from_env()
+        for container_name in container_names:
+            try:
+                container = client.containers.get(container_name)
+                return True
+            except docker.errors.NotFound:
+                return False
+    except docker.errors.APIError as e:
+        print(f"Error connecting to the Docker daemon: {e}")
+
+def time_execution(func, *args, **kwargs):
+    """
+    Measures the time taken by a function to execute.
+
+    Parameters:
+        func (function): The target function to be executed.
+        *args: Positional arguments to be passed to the function.
+        **kwargs: Keyword arguments to be passed to the function.
+
+    Returns:
+        tuple: A tuple containing the result of the function and the time taken to execute it (in seconds).
+    """
+    start_time = time.time()
+    result = func(*args, **kwargs)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    if(result != None):
+        print("Result:", result)
+    print("Time taken: {:.3f} seconds".format(elapsed_time))
+    return result, elapsed_time
 
 
 def deploy_fabric_network():
-    change_to_root_dir()
-    print("Deploying Fabric Network")
-    os.chdir("Masters-v2/Fabric/fabric-network-cc/")
-    command = 'python3 deploy_fabric.py'
-    process = subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    print("Fabric Network deployed")
+    containers = ["ca_orderer"]
+    if not are_containers_deployed(containers):
+        change_to_root_dir()
+        print("Deploying Fabric Network")
+        os.chdir("Masters-v2/Fabric/fabric-network-cc/")
+        command = 'python3 deploy_fabric.py'
+        process = subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        print("Fabric Network deployed")
 
 def deploy_ipfs_node():
-    change_to_root_dir()
-    print("Deploying IPFS Node")
-    os.chdir("Masters-v2/IPFS/")
-    command = 'docker run -it -d \
-                --name ipfs \
-                --network docker_default \
-                -p 4001:4001 \
-                -p 5002:5002 \
-                -p 8070:8070 \
-                yeasy/ipfs'
-    process = subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    print("IPFS Node deployed")
+    containers = ["ipfs"]
+    if not are_containers_deployed(containers):
+        change_to_root_dir()
+        print("Deploying IPFS Node")
+        os.chdir("Masters-v2/IPFS/")
+        command = 'docker run -it -d \
+                    --name ipfs \
+                    --network docker_default \
+                    -p 4001:4001 \
+                    -p 5002:5002 \
+                    -p 8070:8070 \
+                    yeasy/ipfs'
+        process = subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        print("IPFS Node deployed")
 
 def deploy_gateway():
-    change_to_root_dir()
-    print("Deploying Gateway")
-    os.chdir("Masters-v2/Gateway/")
-    command = 'python3 initGateway.py'
-    process = subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    print("Gateway deployed")
+    containers = ["ditto-ipfs-fabric"]
+    if not are_containers_deployed(containers):
+        change_to_root_dir()
+        print("Deploying Gateway")
+        os.chdir("Masters-v2/Gateway/")
+        command = 'python3 initGateway.py'
+        process = subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        print("Gateway deployed")
 
 def deploy_smartdevice():
-    change_to_root_dir()
-    print("Deploying Smart Device")
-    os.chdir("Masters-v2/SmartDevice/")
-    command = 'python3 initSD.py'
-    process = subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    print("Smart Device deployed")    
+    containers = ["iwatch-container"]
+    if not are_containers_deployed(containers):
+        change_to_root_dir()
+        print("Deploying Smart Device")
+        os.chdir("Masters-v2/SmartDevice/")
+        command = 'python3 initSD.py'
+        process = subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        print("Smart Device deployed")    
 
 
 
 def main():
     #REMOVE ALL DOCKER CONTAINERS
-    remove_docker_containers()
+    #remove_docker_containers()
 
     #DEPLOY FABRIC NETWORK
-    deploy_fabric_network()
-
+    #deploy_fabric_network()
+    result, elapsed_time = time_execution(deploy_fabric_network)
+  
     #DEPLOY IPFS Node
-    deploy_ipfs_node()
+    result, elapsed_time = time_execution(deploy_ipfs_node)
 
     #DEPLOY GATEWAY
-    #deploy_gateway()
+    result, elapsed_time = time_execution(deploy_gateway)
 
     #DEPLOY SMART DEVICE
-    #deploy_smartdevice()
-
+    result, elapsed_time = time_execution(deploy_smartdevice)
 
 
 
